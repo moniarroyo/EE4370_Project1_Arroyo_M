dabde0612468ffc99be07cdde528f7226bf78926
moniarroyo <ma1319@txstate.edu>
2015-02-27 16:06:42 -0600

    message

--- MatlabCode1.m
+++ MatlabCode1.m
@@ -1,18 +1,22 @@
-% EExxxx Project 1
-% MATLAB code for xxxxxxxxxxxxx
-% Your Name and Last Name
-% Date
+% EE3370 Project 1
+% MATLAB code for Project 1
+% Monica Arroyo
+% Date 2/27/15
 
 clc      % Clear screen 
 clear    % Clear all variables
 A = 3; f= 1000; omega = 2*pi*f; 
-angle = degtorad(33); T=1/f;
+angle = (33/180)*pi; T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+u_t=circshift(heaviside(t),[1,1]).*heaviside(t);
+x_t2 = cos(omega*t).*sin(omega*t).*u_t;
+subplot(4,1,4)
+plot(t,x_t2,'linewidth',2);grid on
